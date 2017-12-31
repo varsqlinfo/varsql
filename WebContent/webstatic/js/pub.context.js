b724c3f9dc44d9ccacb0f4a585eb2b6cdc86fcac
Administrator <Administrator@WIN-TKSGLC3TS3M>
2018-01-01 01:16:24 +0900

    context 모듈 추가.

--- WebContent/webstatic/js/pub.context.js
+++ WebContent/webstatic/js/pub.context.js
@@ -7,13 +7,14 @@
  * url : https://github.com/ytechinfo/pub
  * demo : http://pub.moaview.com/
 */
-;(function ($, window, document, undefined) {
+;(function ($) {
 
 var pluginName = "pubContextMenu"
 	,initialized = false
 	,_datastore = {}
 	,pubContextElement= false
 	,isContextView = false
+	,_$win = $(window)
 	,defaults = {
 		fadeSpeed: 100			
 		,filter: function ($obj) {
@@ -72,13 +73,13 @@ function Plugin(element, options) {
 Plugin.prototype ={
 	init :function(){
 		var _this = this;
-		defaults = this.options;
-		
+
 	}
 	,initEvt : function (){
+		var _this = this; 
 		$(document).on('mousedown.pubcontext', 'html', function (e) {
 			if(e.which !==2 && $(e.target).closest('#pub-context-area').length < 1){
-				$('#pub-context-area .pub-context').fadeOut(defaults.fadeSpeed, function(){
+				$('#pub-context-area .pub-context').fadeOut(_this.options.fadeSpeed, function(){
 					var sEle = $(this);
 					sEle.css({display:''}).find('.pub-context-sub.drop-left').css('left','').removeClass('drop-left');
 				});
@@ -89,22 +90,34 @@ Plugin.prototype ={
 		var _this = this;
 		var id=_this.contextId;
 
-		if(defaults.preventDoubleContext){
+		if(_this.options.preventDoubleContext){
 			$(document).on('contextmenu.pubcontext'+_this.contextId, '#'+id+'_wrap .pub-context', function (e) {
 				e.preventDefault();
 			});
 		}
 
-		$(document).on('mouseenter.pubcontext'+this.contextId, '#'+id+'_wrap .pub-context-submenu', function(){
+		$(document).on('mouseenter.pubcontext'+this.contextId, '#'+id+'_wrap .pub-context-submenu', function(e){
 			var sEle = $(this); 
-			var $sub = sEle.find('.pub-context-sub:first'),
-				subWidth = $sub.width(),
-				subLeft = $sub.offset().left,
-				collision = (subWidth+subLeft) > window.innerWidth;
-	
+			var $sub = sEle.find('.pub-context-sub:first')
+				, subWidth = $sub.width()
+				, subLeft = $sub.offset().left
+				, collision = (subWidth+subLeft) > _$win.width();
+				
 			if(collision){
-				$sub.css('left', '-'+(subWidth/sEle.width()*100)+'%');
+				$sub.css('left' , '-'+(subWidth/sEle.width()*100)+'%');
 				$sub.addClass('drop-left');
+			}
+			
+			var offTop = sEle.offset().top
+				, subHeight =$sub.height()
+				, screenBottom = (_$win.scrollTop() + _$win.height())
+
+			if(offTop + subHeight+10> screenBottom){
+				offTop = ((offTop + subHeight +10) -screenBottom);
+				offTop = offTop < 0 ? 0 : offTop;
+				$sub.css('top' ,'-'+ offTop+'px');
+			}else{
+				$sub.css('top' ,'');
 			}
 		});
 	}
@@ -112,15 +125,15 @@ Plugin.prototype ={
 		defaults = $.extend({}, defaults, opts);
 	}
 	,buildMenu : function (data, id, subMenu, depth){
-		var _self = this; 
+		var _this = this; 
 		var subClass = (subMenu) ? ' pub-context-sub' : 'pub-context pub-context-top',
-			compressed = defaults.compress ? ' compressed-context' : '',
+			compressed = _this.options.compress ? ' compressed-context' : '',
 			$menuHtm = [];
 		
 		$menuHtm.push('<ul class="pub-context-menu ' + subClass + compressed+'" id="' + id + '">');
  
-		var item ,linkTarget = '',itemKey , styleClass;
-		for(var i = 0; i<data.length; i++) {
+		var dateLen =data.length,item ,linkTarget = '',itemKey , styleClass;
+		for(var i = 0; i< dateLen ; i++) {
 			item = data[i];
 			
 			styleClass = item.styleClass?item.styleClass:'';
@@ -136,7 +149,7 @@ Plugin.prototype ={
 			}
 			
 			itemKey = depth+'_'+item.key; 
-			_self.contextData[itemKey] = item;
+			_this.contextData[itemKey] = item;
 		
 			if (typeof item.target !== 'undefined') {
 				linkTarget = ' target="'+item.target+'"';
@@ -151,7 +164,7 @@ Plugin.prototype ={
 			}
 
 			if (typeof item.subMenu != 'undefined') {
-				var subMenuData = _self.buildMenu(item.subMenu, id, true,depth+1);
+				var subMenuData = _this.buildMenu(item.subMenu, id, true,depth+1);
 				$menuHtm.push(subMenuData);
 			}
 			
@@ -170,10 +183,10 @@ Plugin.prototype ={
 	* 컨텍스트 메뉴 이벤트 처리. 
 	*/
 	,contextEvent : function (){
-		var _self = this,opt = _self.options;
+		var _this = this,opt = _this.options;
 		
-		$('#'+_self.contextId+' .pub-context-item').off('click.'+this.contextId);
-		$('#'+_self.contextId+' .pub-context-item').on('click.'+this.contextId,function (e){
+		$('#'+_this.contextId+' .pub-context-item').off('click.'+this.contextId);
+		$('#'+_this.contextId+' .pub-context-item').on('click.'+this.contextId,function (e){
 			var clickEle=$(this);
 			
 			if(clickEle.hasClass('pub-context-submenu')){
@@ -182,9 +195,9 @@ Plugin.prototype ={
 				skey = clickEle.attr('context-key');
 				var sobj = {
 					key : skey
-					,item : _self.contextData[skey]
-					,list : _self.contextData
-					,element : _self.selectElement
+					,item : _this.contextData[skey]
+					,list : _this.contextData
+					,element : _this.selectElement
 					,evt : e
 				}
 			
@@ -193,16 +206,16 @@ Plugin.prototype ={
 				}else{
 					alert(skey);
 				}
-				_self.closeContextMenu();
+				_this.closeContextMenu();
 			}
 		});
 	}
 	,addContext : function (){
-		var _self = this; 
-		var id = _self.contextId
-			,opt = _self.options
-			,$menu =_self.buildMenu(opt.items, id, false, 0)
-			,selector = _self.selector;
+		var _this = this; 
+		var id = _this.contextId
+			,opt = _this.options
+			,$menu =_this.buildMenu(opt.items, id, false, 0)
+			,selector = _this.selector;
 		
 		$menu = '<div id="'+id+'_wrap" onselectstart="return false" draggable="false">'+$menu+'</div>';
 		
@@ -214,23 +227,22 @@ Plugin.prototype ={
 		
 		pubContextElement.append($menu);
 		
-		_self.contextEvent();
-		var $win = $(window);
-
-		$(document).off('contextmenu.pubcontext'+this.contextId, _self.selector);
-		$(document).on('contextmenu.pubcontext'+this.contextId, _self.selector, function (e) {
+		_this.contextEvent();
+		
+		$(document).off('contextmenu.pubcontext'+this.contextId, _this.selector);
+		$(document).on('contextmenu.pubcontext'+this.contextId, _this.selector, function (e) {
 			e.preventDefault();
 			e.stopPropagation();
 			
-			_self.closeContextMenu();
+			_this.closeContextMenu();
 
 			isContextView = true;
 			//  이전 선택한 클래스 삭제 . 
-			_self.selectElement.removeClass(opt.selectCls);
+			_this.selectElement.removeClass(opt.selectCls);
 
 			var clickObj = $(this); //$( e.target ).closest( e.data.selector );
 			clickObj.addClass(opt.selectCls);
-			_self.selectElement = clickObj; 
+			_this.selectElement = clickObj; 
 			
 			var $dd = $('#'+ id);
 			
@@ -243,29 +255,24 @@ Plugin.prototype ={
 				,evtX = e.pageX
 				,evtY=e.pageY;
 			
-			var bottom = $win.scrollTop() + $win.height(),
-				right = $win.scrollLeft() + $win.width(),
+			var bottom = _$win.scrollTop() + _$win.height(),
+				right = _$win.scrollLeft() + _$win.width(),
 				height =eleH,
 				width = eleW;
 			
-			var offset = {top : evtY , left: evtX};
-			if (offset.top + height > bottom) {
-				offset.top -= height;
-			}
+			var offTop = evtY
+				,offLeft = evtX;
+			
+			offTop = ((offTop + height +20 > bottom )? (bottom- (height+20)) : offTop);
+			offTop = offTop < 0 ? 0 : offTop;
+			
+			offLeft = (offLeft + width > right)? (offLeft-width) : offLeft;
+			offLeft = offLeft < 0 ? 0 : offLeft;
 
-			if (offset.top < 0) {
-				offset.top = 0;
-			}
+			//var log  = {evtX : evtX, evtY : evtY, bottom : bottom, right : right, height : height, width : offTop, offTop : offTop,offLeft: offLeft}
+			//console.log(JSON.stringify(log))
 
-			if (offset.left + width > right) {
-				offset.left -= width;
-			}
-
-			if (offset.left < 0) {
-				offset.left = 0;
-			}
-
-			$dd.css(offset).fadeIn(defaults.fadeSpeed);
+			$dd.css({top : offTop , left: offLeft}).fadeIn(opt.fadeSpeed);
 		});
 	}
 	,destory:function (){
@@ -324,4 +331,4 @@ $[ pluginName ] = function (selector,options) {
 	return _cacheObject;	
 };
 
-})(jQuery, window, document);
+})(jQuery);
