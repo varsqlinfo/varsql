ab26da9805beec3cc73bbc689b5f9f2942fd4d96
ytkim <111>
2019-06-09 13:09:41 +0900

    불필요한 부분 수정.

--- src/$$export/Test.java
+++ src/$$export/Test.java
@@ -4,17 +4,20 @@ import org.stringtemplate.v4.ST;
 import org.stringtemplate.v4.STGroupString;
 import org.stringtemplate.v4.ST.DebugState;
 
+import com.varsql.app.util.VarQueryUtil;
+import com.varsql.app.util.VarsqlUtil;
+
 public class Test {
 	public static void main(String[] args) {
 		
-		String template = "jdbc:oracle:thin:@{serverip}:{port}:{databaseName}"; 
+		String template = "jdbc:oracle:thin:@{serverip}{if(port)}:{port}{endif}:{databaseName}"; 
 		
 		
 		ST bbb = new ST(template, '{', '}');
 		//bbb.debugState = new DebugState();
 		
 		bbb.add("serverip", "11111");
-		bbb.add("port", "222");
+		bbb.add("port", VarQueryUtil.isNumber("-1")?111:false);
 		bbb.add("databaseName", "33");
 		
 		
--- src/com/varsql/app/common/constants/WebStaticResourceVersion.java
+++ src/com/varsql/app/common/constants/WebStaticResourceVersion.java
@@ -4,5 +4,5 @@ public interface WebStaticResourceVersion {
 	final String CODE_MIRROR ="5.43";	
 	final String PRETTIFY ="20180725";	
 	final String PUB_JS =System.currentTimeMillis()+"";	
-	final String VARSQL_CSS =System.currentTimeMillis()+"2";	
+	final String VARSQL_CSS =System.currentTimeMillis()+"";	
 }
